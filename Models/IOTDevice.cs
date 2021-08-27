782001c285fb1e179c1a513f193f9de2ef87f32e
paula <softapps680@gmail.com>
2021-08-27 14:37:25 +0200

    Add project files.

---
+++ Models/IOTDevice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDeviceManager.Models
+{
+
+    public class IOTDevice
+    {
+        public string DeviceId { get; set; }
+        public string DeviceType { get; set; }
+      
+        public string ConnectionState { get; set; }
+        public string Status { get; set; }
+          public string JsonData { get; set; }
+       public DateTime JsonDataLastUpdated { get; set; }
+
+    }
+
+   
+
+}
